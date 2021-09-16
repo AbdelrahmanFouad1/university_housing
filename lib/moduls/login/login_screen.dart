52ee89b59bb4f5b8285030114055d15c66746d96
FarahMohamedEid <FarahMohamedEid>
2021-09-16 14:01:38 +0200

    login_finish2

--- lib/moduls/login/login_screen.dart
+++ lib/moduls/login/login_screen.dart
@@ -10,6 +10,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: backGround,
       body: SingleChildScrollView(
