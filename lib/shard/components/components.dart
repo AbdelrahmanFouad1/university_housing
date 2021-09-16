931111a02b40f585fd4f1151985067d294a29635
Abd El Rahman Fouad <abdofouad.cs@gmail.com>
2021-09-16 23:14:46 +0200

    upgrade

--- lib/shard/components/components.dart
+++ lib/shard/components/components.dart
@@ -15,3 +15,62 @@ void navigateAndFinish(context, widget) => Navigator.pushAndRemoveUntil(
       ),
       (route) => false,
 );
+
+Widget defaultButton({
+  double width = double.infinity,
+  bool isUpperCase = true,
+  double radius = 3.0,
+  double fontSize = 20,
+  required VoidCallback function,
+  required String text,
+}) =>
+    Container(
+      width: width,
+      height: 50.0,
+      child: MaterialButton(
+        onPressed: function,
+        child: Text(
+          isUpperCase ? text.toUpperCase() : text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: fontSize,
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          radius,
+        ),
+        color: mainColors,
+      ),
+    );
+
+Widget defaultButton2({
+  double? width ,
+  double? height,
+  bool isUpperCase = true,
+  double? radius = 8.0,
+  double? fontSize ,
+  required VoidCallback function,
+  required String text,
+}) =>
+    Container(
+      width: width,
+      height: 50.0,
+      child: MaterialButton(
+        onPressed: function,
+        child: Text(
+          isUpperCase ? text.toUpperCase() : text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: fontSize,
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          radius!,
+        ),
+        color: mainColors,
+      ),
+    );
