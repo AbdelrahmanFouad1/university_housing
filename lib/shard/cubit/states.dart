8354450783b452b4fc14d303e222bb53a4829904
Abd El Rahman Fouad <abdofouad.cs@gmail.com>
2021-09-19 23:42:59 +0200

    update hosting request

--- lib/shard/cubit/states.dart
+++ lib/shard/cubit/states.dart
@@ -2,4 +2,10 @@ abstract class AppStates {}
 
 class AppInitialState extends AppStates {}
 
+class ImagePickedSuccessState extends AppStates {}
+
+class ImagePickedErrorState extends AppStates {}
+
+class ChangeStudentState extends AppStates {}
+
 
