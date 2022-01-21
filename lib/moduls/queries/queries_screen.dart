efe82454dd6b07fff049a4b0cd00647917f166b8
FarahMohamedEid <FarahMohamedEid>
2022-01-21 22:01:55 +0200

    finish JAN,21 finish

--- lib/moduls/queries/queries_screen.dart
+++ lib/moduls/queries/queries_screen.dart
@@ -112,7 +112,7 @@ class QueriesScreen extends StatelessWidget {
                       shrinkWrap: true,
                       physics: const BouncingScrollPhysics(),
                       itemBuilder: (context, index) => buildComment(context, AppCubit.get(context).commentModel![index]),
-                      separatorBuilder: (context, index) => const SizedBox(height: 10.0,),
+                      separatorBuilder: (context, index) => const SizedBox(height: 0,),
                       itemCount: AppCubit.get(context).commentModel!.length,
                     ),
                     // if(cubit.commentModel!.isEmpty)
@@ -130,139 +130,148 @@ class QueriesScreen extends StatelessWidget {
       },
     );
   }
-  Widget buildComment(context, CommentsModel model) => Stack(
-    children: [
-      Container(
-        width: double.infinity,
-        height: 150.0,
-        decoration: BoxDecoration(
-            color: ThemeCubit.get(context).darkTheme? finesColorDark : Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
-            boxShadow: [
-              BoxShadow(
-                color:ThemeCubit.get(context).darkTheme? Colors.indigo.withOpacity(0.2) : Colors.grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 2,
-                offset: const Offset(3,3), // changes position of shadow
-              ),
-            ]
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+  Widget buildComment(context, CommentsModel model) {
+    if(model.user != null){
+      return Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 150.0,
+            margin: const EdgeInsets.symmetric(vertical:5.0),
+            decoration: BoxDecoration(
+                color: ThemeCubit.get(context).darkTheme? finesColorDark : Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color:ThemeCubit.get(context).darkTheme? Colors.indigo.withOpacity(0.2) : Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    offset: const Offset(3,3), // changes position of shadow
+                  ),
+                ]
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Builder(builder:(context){
-                  if(model.user == null){
-                    return CircleAvatar(
-                      radius: 25.0,
-                      backgroundColor: ThemeCubit.get(context).darkTheme
-                          ? mainTextColor
-                          : mainColors,
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 80.0,
-                        child: Icon(Icons.error,
-                          color: ThemeCubit.get(context).darkTheme
-                              ? mainColors
-                              : mainTextColor,
-                        ),
-                      ),
-                    );
-                  }else{
-                    if(model.user!.image != null){
-                      return CircleAvatar(
-                        radius: 25.0,
-                        backgroundColor: ThemeCubit.get(context).darkTheme
-                            ? mainTextColor
-                            : mainColors,
-                        backgroundImage: NetworkImage(
-                          model.user!.image!,
-                        ),
-                      );
-                    }else{
-                      return CircleAvatar(
-                        radius: 25.0,
-                        backgroundColor: ThemeCubit.get(context).darkTheme
-                            ? mainTextColor
-                            : mainColors,
-                        child: Container(
-                          alignment: Alignment.center,
-                          height: 80.0,
-                          child: Icon(Icons.error,
-                            color: ThemeCubit.get(context).darkTheme
-                                ? mainColors
-                                : mainTextColor,
+                Row(
+                  children: [
+                    Builder(builder:(context){
+                      if(model.user == null){
+                        return CircleAvatar(
+                          radius: 25.0,
+                          backgroundColor: ThemeCubit.get(context).darkTheme
+                              ? mainTextColor
+                              : mainColors,
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 80.0,
+                            child: Icon(Icons.error,
+                              color: ThemeCubit.get(context).darkTheme
+                                  ? mainColors
+                                  : mainTextColor,
+                            ),
+                          ),
+                        );
+                      }else{
+                        if(model.user!.image != null){
+                          return CircleAvatar(
+                            radius: 25.0,
+                            backgroundColor: ThemeCubit.get(context).darkTheme
+                                ? mainTextColor
+                                : mainColors,
+                            backgroundImage: NetworkImage(
+                              model.user!.image!,
+                            ),
+                          );
+                        }else{
+                          return CircleAvatar(
+                            radius: 25.0,
+                            backgroundColor: ThemeCubit.get(context).darkTheme
+                                ? mainTextColor
+                                : mainColors,
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 80.0,
+                              child: Icon(Icons.error,
+                                color: ThemeCubit.get(context).darkTheme
+                                    ? mainColors
+                                    : mainTextColor,
+                              ),
+                            ),
+                          );
+                        }
+                      }
+                    }),
+
+                    const SizedBox(width: 10.0,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          model.user == null ? 'فارغ' : model.user!.username!,
+                          style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                      );
-                    }
-                  }
-                }),
-
-                const SizedBox(width: 10.0,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      model.user == null ? 'فارغ' : model.user!.username!,
-                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                        fontWeight: FontWeight.bold,
+                        Text(
+                          model.user == null ? 'فارغ' :'${model.user!.id!}',
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    if(model.isReplied!)
+                      Container(
+                        padding: const EdgeInsetsDirectional.all(8.0),
+                        child: Icon(
+                          Icons.check_circle,
+                          color: ThemeCubit.get(context).darkTheme? mainTextColor : mainColors,
+                          size: 16.0,
+                        ),
                       ),
-                    ),
-                    Text(
-                      model.user == null ? 'فارغ' :'${model.user!.id!}',
-                      style: Theme.of(context).textTheme.bodyText2,
-                    ),
                   ],
                 ),
-                const Spacer(),
-                if(model.isReplied!)
-                  Container(
-                  padding: const EdgeInsetsDirectional.all(8.0),
-                  child: Icon(
-                    Icons.check_circle,
-                    color: ThemeCubit.get(context).darkTheme? mainTextColor : mainColors,
-                    size: 16.0,
+                const SizedBox(height: 5.0,),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsetsDirectional.only(start: 8.0,end: 8.0),
+                  child: Text(
+                    model.enquiry!,
+                    style: Theme.of(context).textTheme.bodyText2,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                InkWell(
+                  onTap: (){
+                    navigateTo(context,  QueriesDetailsScreen(commentsModel: model));
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsetsDirectional.only(end: 5.0),
+                    child: Text(
+                      'المزيد',
+                      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        fontSize: 14.0,
+                        decoration:TextDecoration.underline,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 5.0,),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsetsDirectional.only(start: 8.0,end: 8.0),
-              child: Text(
-                model.enquiry!,
-                style: Theme.of(context).textTheme.bodyText2,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            const SizedBox(height: 10,),
-            InkWell(
-              onTap: (){
-                navigateTo(context,  QueriesDetailsScreen(commentsModel: model));
-              },
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsetsDirectional.only(end: 5.0),
-                child: Text(
-                  'المزيد',
-                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                    fontSize: 14.0,
-                    decoration:TextDecoration.underline,
-                  ),
-                  textAlign: TextAlign.end,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ],
-  );
+          ),
+        ],
+      );
+    }else{
+      return Container(
+        width: 0,
+      );
+    }
+  }
 }
